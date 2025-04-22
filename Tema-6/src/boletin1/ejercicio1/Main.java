3534b883397b7c4b86092c2cce7d71288baa6724
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-04-22 13:45:47 +0200

    Ejercicio 1 terminado.

---
+++ Tema-6/src/boletin1/ejercicio1/Main.java
@@ -0,0 +1,31 @@
+package boletin1.ejercicio1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double suma = 0;
+		double contador = 0;
+		
+		try (Scanner sc = new Scanner(new FileReader("src\\boletin1\\ejercicio1\\NumerosReales.txt"))) {
+			
+			// Mientras haya un siguiente número...
+			while (sc.hasNextDouble()) {
+				suma += sc.nextDouble(); // Añadimos el siguiente número
+				contador++; // Incrementamos el contador para la media
+			}
+			
+			// Mostrar por pantalla los resultados
+			System.out.println("Suma: " + suma + "\nMedia: " + suma / contador);
+			
+		} catch (FileNotFoundException e) { // Recogemos la excepción en caso de que se lance
+			System.err.println("Archivo no encontrado.");
+		}
+
+	}
+
+}
