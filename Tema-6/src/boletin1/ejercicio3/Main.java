2e6030c4ba34762578a59d46829cc157ff6b1dee
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-04-23 08:37:28 +0200

    Ejercicio 3 terminado.

--- Tema-6/src/boletin1/ejercicio3/Main.java
+++ Tema-6/src/boletin1/ejercicio3/Main.java
@@ -11,21 +11,39 @@ public class Main {
 		int edad;
 		double altura;
 		
-		int sumaEdad;
-		double sumaAltura;
-		int contador;
+		int sumaEdad = 0;
+		double sumaAltura = 0;
+		int contador = 0;
 		
 		try (Scanner sc = new Scanner(new FileReader("src\\boletin1\\ejercicio3\\Alumnos.txt"))) {
 			while (sc.hasNextLine()) {
+				int i = 1;
+				
 				String datos[] = {sc.next(), sc.next(), sc.next()};
 				
 				for (String dato : datos) {
-					int i = 1;
 					System.out.print(dato + " ");
+					
+					switch (i) {
+					case 1 -> {
+						contador++;
+					}
+					case 2 -> {
+						sumaEdad += Integer.parseInt(dato);
+					}
+					case 3 -> {
+						sumaAltura += Double.parseDouble(dato);
+					}
+					}
+					
+					i++;
 				}
 				
 				System.out.println();
 			}
+			
+			System.out.println(sumaEdad + "\n" + sumaAltura);
+			
 		} catch (FileNotFoundException e) {
 			System.err.println("Archivo no encontrado.");
 		}
