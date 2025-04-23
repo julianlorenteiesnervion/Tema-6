ea8e5de3270ff0dd1eec6c032c7dda82fcd4b293
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-04-23 09:30:14 +0200

    Ejercicio 5 terminado.

--- Tema-6/src/boletin1/ejercicio5/Main.java
+++ Tema-6/src/boletin1/ejercicio5/Main.java
@@ -1,9 +1,28 @@
 package boletin1.ejercicio5;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("src\\boletin1\\ejercicio5\\datos.txt", true))) {
+			Scanner reader = new Scanner(System.in);
+			
+			System.out.print("Nombre: ");
+			bw.write(reader.nextLine());
+			
+			System.out.print("Edad: ");
+			bw.write(" " + reader.nextInt());
+			
+			bw.newLine();
+			
+			reader.close();
+			
+		} catch (IOException e) {
+			System.err.println("Archivo no encontrado.");
+		}
 
 	}
 
